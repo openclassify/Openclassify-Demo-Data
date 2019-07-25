d5ccda274dbc22ba32ac391a95e7cd5f4077d198
spektra2147 <39536659+spektra2147@users.noreply.github.com>
2019-07-25 12:30:38 +0300

    Update store.sql

--- store.sql
+++ store.sql
@@ -10,12 +10,12 @@ SET time_zone = "+00:00";
 
 
 INSERT INTO `default_files_files` (`id`, `sort_order`, `created_at`, `created_by_id`, `updated_at`, `updated_by_id`, `deleted_at`, `name`, `disk_id`, `folder_id`, `extension`, `size`, `mime_type`, `entry_id`, `entry_type`, `keywords`, `height`, `width`, `alt_text`, `title`, `caption`, `description`) VALUES
-(128, 45, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'electronics.png', 1, 4, 'png', 156843, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
-(129, 46, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'realestate2.png', 1, 4, 'png', 121117, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
-(130, 47, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'clothes2.png', 1, 4, 'png', 262373, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
-(131, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'realestate.png', 1, 4, 'png', 66675, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '333', '1000', NULL, NULL, NULL, NULL),
-(132, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'electronics2.png', 1, 4, 'png', 189120, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
-(133, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'clothes.png', 1, 4, 'png', 213372, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL);
+(128, 45, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'electronics.png', 1, 12, 'png', 156843, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
+(129, 46, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'realestate2.png', 1, 12, 'png', 121117, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
+(130, 47, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'clothes2.png', 1, 12, 'png', 262373, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
+(131, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'realestate.png', 1, 12, 'png', 66675, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '333', '1000', NULL, NULL, NULL, NULL),
+(132, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'electronics2.png', 1, 12, 'png', 189120, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
+(133, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'clothes.png', 1, 12, 'png', 213372, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
