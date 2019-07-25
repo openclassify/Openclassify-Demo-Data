ffb00eafcae72f7d6eccd30666e2ed131b1cd25c
vedatakd <vedat@vebze.com>
2019-07-25 12:17:38 +0300

    add store banner

---
+++ store.sql
@@ -0,0 +1,23 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+INSERT INTO `default_files_files` (`id`, `sort_order`, `created_at`, `created_by_id`, `updated_at`, `updated_by_id`, `deleted_at`, `name`, `disk_id`, `folder_id`, `extension`, `size`, `mime_type`, `entry_id`, `entry_type`, `keywords`, `height`, `width`, `alt_text`, `title`, `caption`, `description`) VALUES
+(128, 45, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'electronics.png', 1, 4, 'png', 156843, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
+(129, 46, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'realestate2.png', 1, 4, 'png', 121117, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
+(130, 47, '2019-07-25 09:00:51', 1, '2019-07-25 09:00:52', 1, NULL, 'clothes2.png', 1, 4, 'png', 262373, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
+(131, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'realestate.png', 1, 4, 'png', 66675, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '333', '1000', NULL, NULL, NULL, NULL),
+(132, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'electronics2.png', 1, 4, 'png', 189120, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL),
+(133, 48, '2019-07-25 09:00:52', 1, '2019-07-25 09:00:53', 1, NULL, 'clothes.png', 1, 4, 'png', 213372, 'image/png', NULL, 'Anomaly\\Streams\\Platform\\Model\\Files\\FilesStoreBannerFolderEntryModel', NULL, '334', '1000', NULL, NULL, NULL, NULL);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
